01a51cb0687efec4b6da5ca0fbdc22a201dcdc03
syntheticdev <35655841+syntheticdev@users.noreply.github.com>
2020-10-04 20:49:48 +0100

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -23,14 +23,27 @@ class Settings {
         console.log(`Made settings for guild ${guild.id}`)
     }
 
+    /**
+     * Checks if a setting exists
+     * @param {string} name 
+     */
     isSetting(name) {
         if (!this.settings.has(name)) console.error(`No setting called '${name}' exists`);
     }
 
+    /**
+     * Checks if a setting has a default value
+     * @param {Object} setting 
+     * @param {string} name 
+     */
     hasDefault(setting, name) {
         if (!Object.keys(setting).includes("defaultValue")) console.error(`Setting '${name}' does not have a defaultValue`);
     }
 
+    /**
+     * Get a setting's value
+     * @param {string} name 
+     */
     async getSetting(name) {
         this.isSetting(name)
 
@@ -44,15 +57,20 @@ class Settings {
             this.hasDefault(setting, name)
 
             data = new model({
-                Value: setting.defaultValue,
+                Value: setting.jsonFormatted ? JSON.stringify(setting.defaultValue) : setting.defaultValue,
                 GuildID: this.guild.id
             })
             data.save()
         }
 
-        return data.Value
+        return setting.jsonFormatted ? JSON.parse(data.Value) : data.Value
     }
 
+    /**
+     * Set a setting's value
+     * @param {string} name 
+     * @param {*} value 
+     */
     async setSetting(name, value) {
         this.isSetting(name)
 
@@ -63,13 +81,52 @@ class Settings {
         })
 
         if (data) {
-            await model.findOneAndRemove({
+            await model.findOneAndDelete({
                 GuildID: this.guild.id
             })
         }
 
         data = new model({
-            Value: value,
+            Value: setting.jsonFormatted ? JSON.stringify(value) : value,
+            GuildID: this.guild.id
+        })
+        data.save()
+    }
+
+    /**
+     * Sets a setting's value to the returned value of the transform
+     * @param {string} name 
+     * @param {Function} transform 
+     */
+    async editSetting(name, transform) {
+        this.isSetting(name)
+
+        const setting = this.settings.get(name)
+        const model = setting.model
+        let data = await model.findOne({
+            GuildID: this.guild.id
+        })
+
+        if (!data) {
+            this.hasDefault(setting, name)
+
+            data = new model({
+                Value: setting.jsonFormatted ? JSON.stringify(setting.defaultValue) : setting.defaultValue,
+                GuildID: this.guild.id
+            })
+        }
+
+        let newValue = await transform(setting.jsonFormatted ? JSON.parse(data.Value) : data.Value)
+        newValue = setting.jsonFormatted ? JSON.stringify(newValue) : newValue
+
+        if (!newValue || data.Value == newValue) return;
+
+        await model.findOneAndDelete({
+            GuildID: this.guild.id
+        })
+
+        data = new model({
+            Value: newValue,
             GuildID: this.guild.id
         })
         data.save()
