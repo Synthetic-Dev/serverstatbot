dd1efee0d563629a5a528fb79c314b96cc53ee45
marineboy308 <35655841+marineboy308@users.noreply.github.com>
2021-01-09 14:14:05 +0000

    Update About.js

--- src/commands/About.js
+++ src/commands/About.js
@@ -13,54 +13,52 @@ class Command extends ICommand {
     }
 
     async execute(inputs, message) {
-        this.getServerInfo(message, data => {
-            try {
-                message.channel.send({
-                    embed: {
-                        title: "About",
-                        author: {
-                            name: this.client.user.username,
-                            icon_url: this.client.user.avatarURL({
-                                size: 64,
-                                dynamic: true,
-                                format: "png"
-                            })
+        try {
+            message.channel.send({
+                embed: {
+                    title: "About",
+                    author: {
+                        name: this.client.user.username,
+                        icon_url: this.client.user.avatarURL({
+                            size: 64,
+                            dynamic: true,
+                            format: "png"
+                        })
+                    },
+                    color: 5145560,
+                    fields: [
+                        {
+                            name: "Version",
+                            value: process.env.npm_package_version,
+                            inline: true
                         },
-                        color: 5145560,
-                        fields: [
-                            {
-                                name: "Version",
-                                value: process.env.npm_package_version,
-                                inline: true
-                            },
-                            {
-                                name: "Developed by",
-                                value: "SyntheticDev",
-                                inline: true
-                            },
-                            {
-                                name: "Credit to",
-                                value: "[api.mcsrvstat.us](https://api.mcsrvstat.us/)\n[mc-heads.net](https://mc-heads.net/)",
-                                inline: true
-                            },
-                            {
-                                name: "Servers",
-                                value: this.client.guilds.cache.size
-                            },
-                            {
-                                name: "Users",
-                                value: this.client.users.cache.size,
-                                inline: true
-                            },
-                            {
-                                name: "Invite",
-                                value: "[Invite bot](https://discord.com/oauth2/authorize?client_id=759415210628087841&scope=bot%20identify%20guilds&permissions=1275587792)"
-                            }
-                        ]
-                    }
-                })
-            } catch(e) {console.error(e)}
-        })
+                        {
+                            name: "Developed by",
+                            value: "SyntheticDev",
+                            inline: true
+                        },
+                        {
+                            name: "Credit to",
+                            value: "[api.mcsrvstat.us](https://api.mcsrvstat.us/)\n[mc-heads.net](https://mc-heads.net/)",
+                            inline: true
+                        },
+                        {
+                            name: "Servers",
+                            value: this.client.guilds.cache.size
+                        },
+                        {
+                            name: "Users",
+                            value: this.client.users.cache.size,
+                            inline: true
+                        },
+                        {
+                            name: "Invite",
+                            value: "[Invite bot](https://discord.com/oauth2/authorize?client_id=759415210628087841&scope=bot%20identify%20guilds&permissions=1275587792)"
+                        }
+                    ]
+                }
+            })
+        } catch(e) {console.error(e)}
     }
 }
 
