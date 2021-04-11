fc64e69a50f348a76a34f10fd4883e11ad963377
Syntheticdev <marineboy308@gmail.com>
2021-04-11 23:02:22 +0100

    Push MOTD generator

--- src/utils/mojang.js
+++ src/utils/mojang.js
@@ -1,8 +1,188 @@
+const Canvas = require("canvas")
 const Util = require("./util.js");
 
 class Mojang {
     constructor() {
         console.error(`The ${this.constructor.name} class cannot be constructed.`);
+    }
+
+    static specials = ["§", "&"];
+    static colors = {
+        "0": "#000",
+        "1": "#0000aa",
+        "2": "#00aa00",
+        "3": "#00aaaa",
+        "4": "#aa0000",
+        "5": "#aa00aa",
+        "6": "#ffaa00",
+        "7": "#aaaaaa",
+        "8": "#555555",
+        "9": "#5555ff",
+        "a": "#55ff55",
+        "b": "#55ffff",
+        "c": "#ff5555",
+        "d": "#ff55ff",
+        "e": "#ffff55",
+        "f": "#fff",
+        "g": "#ddd605"
+    }
+    static effects = {
+        "k": (build) => {
+            build.extras.obfuscated = true
+        },
+        "l": (build) => {
+            if (!build.font.includes("bold")) {
+                build.font = "bold " + build.font
+            }
+        },
+        "m": (build) => {
+            build.extras.strikethrough = true
+        },
+        "n": (build) => {
+            build.extras.underline = true
+        },
+        "o": (build) => {
+            if (!build.font.includes("italic")) {
+                build.font = "italic " + build.font
+            }
+        },
+        "r": (build) => {
+            build.extras = {
+                obfuscated: false,
+                underline: false,
+                strikethrough: false
+            }
+            build.fillStyle = this.colors.f
+            build.font = "20px 'Minecraft'"
+        },
+    }
+
+    static getFormatChars() {
+        return this.specials
+    }
+
+    static getColorCodes() {
+        return this.colors
+    }
+
+    static getEffects() {
+        return this.effects
+    }
+
+    static generateMOTD(text, spacing, padding) {
+        let lines = text.split("\n")
+        const tempPadding = padding*1.5
+
+        let motd = Canvas.createCanvas(1024 + tempPadding * 2, 20 * lines.length + spacing * (lines.length - 1) + tempPadding * 2)
+        let context = motd.getContext("2d")
+        context.imageSmoothingEnabled = false
+        context.font = "20px 'Minecraft'"
+        context.textBaseline = "top"
+        context.textAlign = "left"
+        context.fillStyle = "#fff"
+
+        const specials = this.getFormatChars()
+        const colors = this.getColorCodes()
+        const effects = this.getEffects()
+
+        lines.forEach((raw, index) => {
+            let buildList = []
+            let build = {
+                text: "",
+                fillStyle: colors.f,
+                font: "20px 'Minecraft'",
+                extras: {
+                    obfuscated: false,
+                    underline: false,
+                    strikethrough: false
+                },
+                changed: false
+            }
+
+            let pushBuild = () => {
+                if (build.extras.obfuscated) {
+                    let length = build.text.length
+                    build.text = ""
+                    const banned = ["/", "?", ":", ";", ".", ",", "'", "|", "\\", "`"]
+                    const start = 33, end = 126
+                    for (let i = 0; i < length; i++) {
+                        let char
+                        while (!char || banned.includes(char)) {
+                            char = String.fromCharCode(start + Math.floor(Math.random() * (end - start)))
+                        }
+                        build.text += char
+                    }
+                };
+                buildList.push(build)
+            }
+
+            for (let i = 0; i < raw.length; i++) {
+                if (specials.includes(raw[i - 1])) continue;
+                let char = raw[i]
+                if (specials.includes(char)) {
+                    let code = raw[i + 1]
+                    if (code) {
+                        code = code.toLowerCase()
+                        let color = colors[code]
+                        if (color != null) {
+                            if (build.fillStyle != colors.f || build.changed) {
+                                pushBuild()
+                                build = {
+                                    text: "",
+                                    fillStyle: color,
+                                    font: build.font,
+                                    extras: {
+                                        obfuscated: build.extras.obfuscated,
+                                        underline: build.extras.underline,
+                                        strikethrough: build.extras.strikethrough
+                                    },
+                                    changed: false
+                                }
+                            } else build.fillStyle = color;
+                        } else {
+                            let effect = effects[code]
+                            if (effect) {
+                                if (build.changed) {
+                                    pushBuild()
+                                    build = {
+                                        text: "",
+                                        fillStyle: build.fillStyle,
+                                        font: build.font,
+                                        extras: {
+                                            obfuscated: build.extras.obfuscated,
+                                            underline: build.extras.underline,
+                                            strikethrough: build.extras.strikethrough
+                                        },
+                                        changed: false
+                                    }
+                                }
+
+                                effect(build)
+                            } else {
+                                build.changed = true
+                                build.text += char
+                            }
+                        }
+                    } else {
+                        build.changed = true
+                        build.text += char
+                    }
+                } else {
+                    build.changed = true
+                    build.text += char
+                }
+            }
+            if (buildList[buildList.length] != build) pushBuild(build);
+
+            Util.fillMixedText(context, buildList, tempPadding, tempPadding + 20 * index + spacing * index, motd.width)
+        })
+
+        Util.autoCropCanvas(context, padding)
+        context.globalCompositeOperation = "destination-over"
+        context.fillStyle = "#000"
+        context.fillRect(0, 0, motd.width, motd.height)
+
+        return motd
     }
 
     /**
