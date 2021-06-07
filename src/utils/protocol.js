c9868e9c23bd1b3006befbc147f814a68c68cab3
Syntheticdev <marineboy308@gmail.com>
2021-06-07 22:47:50 +0100

    Expose raw forge data to api

--- src/utils/protocol.js
+++ src/utils/protocol.js
@@ -7,25 +7,27 @@ const failStrings = [
     "Failed to retrieve the status of the server within time",
     "Failed to retrieve the status of the server",
     "Failed to query server within time",
-    "Failed to query server"
+    "Failed to query server",
 ]
 
-const cacheTime = 30;
+const cacheTime = 30
 const requestCache = new NodeCache({
     checkperiod: cacheTime / 2,
     stdTTL: cacheTime,
-    useClones: true
-});
+    useClones: true,
+})
 
 let sessionCount = 0
 
 class Protocol {
     constructor() {
-        console.error(`The ${this.constructor.name} class cannot be constructed.`);
+        console.error(
+            `The ${this.constructor.name} class cannot be constructed.`
+        )
     }
 
-    static maxPort = 65535;
-    static blockedHosts = ["0.0.0.0", "localhost", "127.0.0.1"];
+    static maxPort = 65535
+    static blockedHosts = ["0.0.0.0", "localhost", "127.0.0.1"]
 
     /**
      * The minimum minecraft version that the statusFE01() handshake supports
@@ -36,9 +38,14 @@ class Protocol {
     }
 
     static getErrorType(error) {
-        if (!error) return "offline";
-        
-        if (failStrings.includes(error.message) || error.code == "ETIMEDOUT" || error.code == "EHOSTUNREACH" || error.code == "ECONNREFUSED") {
+        if (!error) return "offline"
+
+        if (
+            failStrings.includes(error.message) ||
+            error.code == "ETIMEDOUT" ||
+            error.code == "EHOSTUNREACH" ||
+            error.code == "ECONNREFUSED"
+        ) {
             return "offline"
         } else if (error.code == "ENOTFOUND") {
             return "notfound"
@@ -51,7 +58,8 @@ class Protocol {
     }
 
     static isIpValid(ip) {
-        const validIp_HostnameRegex = /^(((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|((([a-zA-Z]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z]|[A-Za-z][A-Za-z0-9\‌​-]*[A-Za-z0-9])))$/g
+        const validIp_HostnameRegex =
+            /^(((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|((([a-zA-Z]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z]|[A-Za-z][A-Za-z0-9\‌​-]*[A-Za-z0-9])))$/g
         return validIp_HostnameRegex.test(ip)
     }
 
@@ -75,98 +83,129 @@ class Protocol {
     /**
      * Send requests to server to get all available information
      * @private Use getInfo() for formatted data
-     * @param {string} ip 
-     * @param {number} port 
+     * @param {string} ip
+     * @param {number} port
      * @param {number} queryPort
      * @returns {Promise<boolean, Object<string, any> | Error | string>}
      */
     static async sendRequest(ip, port = 25565, queryPort = port) {
-        if (this.blockedHosts.includes(ip.toLowerCase())) return [false, new Error("Blocked host")];
-        if (port > this.maxPort) return [false, new Error("Port exceeds maximum")];
-        if (queryPort > this.maxPort) return [false, new Error("Query port exceeds maximum")];
+        if (this.blockedHosts.includes(ip.toLowerCase()))
+            return [false, new Error("Blocked host")]
+        if (port > this.maxPort)
+            return [false, new Error("Port exceeds maximum")]
+        if (queryPort > this.maxPort)
+            return [false, new Error("Query port exceeds maximum")]
 
-        let bulkData = new Promise(resolve => {
-            let args = [ip, {port: port, timeout: 3000}]
+        let bulkData = new Promise((resolve) => {
+            let args = [ip, { port: port, timeout: 3000 }]
 
             function query(statusResponse) {
-                if (statusResponse) statusResponse.bedrock = !!statusResponse.bedrock;
+                if (statusResponse)
+                    statusResponse.bedrock = !!statusResponse.bedrock
 
                 sessionCount++
                 args[1].port = queryPort
                 args[1].sessionID = sessionCount
                 args[1].timeout = 5000
 
-                McUtilMod.queryFull(...args).then(queryResponse => {
-                    queryResponse.query = true;
-                    queryResponse.ping = statusResponse != null;
+                McUtilMod.queryFull(...args)
+                    .then((queryResponse) => {
+                        queryResponse.query = true
+                        queryResponse.ping = statusResponse != null
 
-                    if (statusResponse) {
-                        Object.keys(statusResponse).forEach(key => {
-                            let value = statusResponse[key]
-                            if (queryResponse[key] == null) {
-                                queryResponse[key] = value
-                            }
-
-                            if (value instanceof MinecraftUtil.Description) {
-                                if (queryResponse[key].descriptionText == "") {
+                        if (statusResponse) {
+                            Object.keys(statusResponse).forEach((key) => {
+                                let value = statusResponse[key]
+                                if (queryResponse[key] == null) {
                                     queryResponse[key] = value
                                 }
-                            }
-                        })
-                    } else {
-                        queryResponse.bedrock = queryResponse.edition == "MCPE" || queryResponse.serverGUID != null
-                    }
-                    resolve([true, queryResponse])
-                }).catch(e => {
-                    if (statusResponse) {
-                        statusResponse.query = false;
-                        statusResponse.ping = true;
-                        resolve([true, statusResponse])
-                    } else resolve([false, e]);
-                })
+
+                                if (
+                                    value instanceof MinecraftUtil.Description
+                                ) {
+                                    if (
+                                        queryResponse[key].descriptionText == ""
+                                    ) {
+                                        queryResponse[key] = value
+                                    }
+                                }
+                            })
+                        } else {
+                            queryResponse.bedrock =
+                                queryResponse.edition == "MCPE" ||
+                                queryResponse.serverGUID != null
+                        }
+                        resolve([true, queryResponse])
+                    })
+                    .catch((e) => {
+                        if (statusResponse) {
+                            statusResponse.query = false
+                            statusResponse.ping = true
+                            resolve([true, statusResponse])
+                        } else resolve([false, e])
+                    })
             }
 
             function attemptBedrock() {
-                McUtilMod.statusBedrock(...args).then(response => {
-                    response.bedrock = true;
-                    query(response)
-                }).catch(() => {query()})
+                McUtilMod.statusBedrock(...args)
+                    .then((response) => {
+                        response.bedrock = true
+                        query(response)
+                    })
+                    .catch(() => {
+                        query()
+                    })
             }
 
             if (port == 19132) attemptBedrock()
             else {
-                McUtilMod.status(...args).then(data => {
-                    query(data)
-                }).catch(e => {
-                    if (!e) query()
-                    else if (e.message == "Failed to retrieve the status of the server within time") {
-                        McUtilMod.statusFE01(...args).then(data => {
-                            query(data)
-                        }).catch(() => {query()})
-                    } else if ((e.code == "ECONNREFUSED" || e.message == "Failed to retrieve the status of the server") && port != 25565) attemptBedrock()
-                    else resolve([false, e]);
-                })
+                McUtilMod.status(...args)
+                    .then((data) => {
+                        query(data)
+                    })
+                    .catch((e) => {
+                        if (!e) query()
+                        else if (
+                            e.message ==
+                            "Failed to retrieve the status of the server within time"
+                        ) {
+                            McUtilMod.statusFE01(...args)
+                                .then((data) => {
+                                    query(data)
+                                })
+                                .catch(() => {
+                                    query()
+                                })
+                        } else if (
+                            (e.code == "ECONNREFUSED" ||
+                                e.message ==
+                                    "Failed to retrieve the status of the server") &&
+                            port != 25565
+                        )
+                            attemptBedrock()
+                        else resolve([false, e])
+                    })
             }
         })
 
         let [success, result] = await bulkData
-        if (!success) return [false, result];
+        if (!success) return [false, result]
 
-        result.cached = false;
-        result.latency = result.roundTripLatency ?? -1;
+        result.cached = false
+        result.latency = result.roundTripLatency ?? -1
         result._description = {
             raw: [],
             text: {
                 raw: "A Minecraft Server",
-                clean: "A Minecraft Server"
+                clean: "A Minecraft Server",
             },
             ansi: null,
-            default: true
+            default: true,
         }
 
-        if (result.motdLine1) result._description.raw.push(result.motdLine1);
-        if (result.motdLine2) result._description.raw.push(result.motdLine2);
-        if (result.description) result._description.raw.push(result.description);
+        if (result.motdLine1) result._description.raw.push(result.motdLine1)
+        if (result.motdLine2) result._description.raw.push(result.motdLine2)
+        if (result.description) result._description.raw.push(result.description)
 
         result.description = result._description
         delete result._description
@@ -175,8 +214,8 @@ class Protocol {
             let desc = result.description
             desc.text.raw = []
             desc.text.clean = []
-            desc.raw.forEach(d => {
-                if (!d) return;
+            desc.raw.forEach((d) => {
+                if (!d) return
                 desc.text.raw.push(d.toString())
                 desc.text.clean.push(d.toRaw())
             })
@@ -191,50 +230,75 @@ class Protocol {
             }
         }
 
+        let forgeData = result?.rawResponse?.forgeData
+        if (forgeData) {
+            if (!result.modInfo) {
+                result.modInfo = forgeData
+                result.modInfo.type = "Forge"
+
+                if (result.modInfo.mods) {
+                    result.modInfo.modList = result.modInfo.mods
+                    delete result.modInfo.mods
+                }
+            } else if (!result.modInfo.modList) {
+                result.modInfo.modList = forgeData.mods
+            }
+        }
+
         return [true, result]
     }
 
     /**
      * Ping a minecraft server to get its details
-     * @param {string} ip 
-     * @param {number} port 
+     * @param {string} ip
+     * @param {number} port
      * @param {number} queryPort
      * @returns {Promise<Object<string, any>}
      */
     static async getInfo(ip, port = 25565, queryPort = port) {
-        if (queryPort < 0) queryPort = port;
+        if (queryPort < 0) queryPort = port
 
         let final
         let cacheKey = ip + ":" + port + "/" + queryPort
         if (requestCache.has(cacheKey)) {
             final = requestCache.get(cacheKey)
-            final.cached = true;
+            final.cached = true
             return final
         }
 
         let [success, result] = await this.sendRequest(ip, port, queryPort)
 
         if (success) {
-            let players = result.players ? result.players : result.samplePlayers ? result.samplePlayers : []
+            let players = result.players
+                ? result.players
+                : result.samplePlayers
+                ? result.samplePlayers
+                : []
             players.forEach((player, index) => {
                 if (typeof player == "string") {
                     players[index] = {
                         id: null,
                         name: {
                             raw: player,
-                            clean: player.replace(/§./g, "").trim()
-                        }
-                    } 
+                            clean: player.replace(/§./g, "").trim(),
+                        },
+                    }
                 } else {
                     const name = player.name
                     player.name = {
                         raw: name,
-                        clean: name.replace(/§./g, "").trim()
+                        clean: name.replace(/§./g, "").trim(),
                     }
                 }
             })
 
-            players.sort((a, b) => (a.name.clean > b.name.clean) ? 1 : ((b.name.clean > a.name.clean) ? -1 : 0))
+            players.sort((a, b) =>
+                a.name.clean > b.name.clean
+                    ? 1
+                    : b.name.clean > a.name.clean
+                    ? -1
+                    : 0
+            )
 
             final = {
                 ip: result.host,
@@ -246,7 +310,10 @@ class Protocol {
                 query: result.query,
                 cached: result.cached,
                 bedrock: result.bedrock,
-                modded: result.modInfo != null && result.modInfo.modList != null && result.modInfo.modList.length > 0,
+                modded:
+                    result.modInfo != null &&
+                    result.modInfo.modList != null &&
+                    result.modInfo.modList.length > 0,
                 srvRecord: result.srvRecord,
                 levelName: result.levelName,
                 edition: result.edition,
@@ -257,22 +324,24 @@ class Protocol {
                 portIPv4: result.portIPv4,
                 portIPv6: result.portIPv6,
                 version: {
-                    minecraft: result.version + (result.software ? ` (${result.software})` : ""),
-                    protocol: result.protocolVersion ?? null
+                    minecraft:
+                        result.version +
+                        (result.software ? ` (${result.software})` : ""),
+                    protocol: result.protocolVersion ?? null,
                 },
                 players: {
                     online: result.onlinePlayers,
                     max: result.maxPlayers,
-                    sample: players
+                    sample: players,
                 },
                 motd: {
                     raw: result.description.text.raw,
                     clean: result.description.text.clean,
-                    ansi: result.description.ansi
+                    ansi: result.description.ansi,
                 },
                 favicon: result.favicon,
                 plugins: result.plugins ?? null,
-                mods: result.modInfo ?? null
+                mods: result.modInfo ?? null,
             }
 
             if (final.bedrock) {
@@ -281,7 +350,7 @@ class Protocol {
             }
 
             if (final.mods) {
-                final.mods.modList.forEach(mod => {
+                final.mods.modList.forEach((mod) => {
                     if (mod.modmarker) {
                         mod.version = mod.modmarker
                         delete mod.modmarker
@@ -291,9 +360,18 @@ class Protocol {
                         mod.modId = mod.modid
                         delete mod.modid
                     }
+
+                    if (/^OHNOES/g.test(mod.version)) {
+                        mod.version = "unknown"
+                    }
                 })
 
-                final.mods.modList = final.mods.modList.filter(mod => !(["minecraft", "fml", "mcp", "forge"].includes(mod.modId.toLowerCase())))
+                final.mods.modList = final.mods.modList.filter(
+                    (mod) =>
+                        !["minecraft", "fml", "mcp", "forge"].includes(
+                            mod.modId.toLowerCase()
+                        )
+                )
             }
 
             if (final.plugins) {
@@ -301,7 +379,7 @@ class Protocol {
                     let full = plugin.toString().trim()
                     final.plugins[index] = {
                         name: full.split(" ").shift(),
-                        version: full.split(" ").splice(1).join(" ")
+                        version: full.split(" ").splice(1).join(" "),
                     }
                 })
             }
@@ -310,7 +388,7 @@ class Protocol {
                 ip: ip,
                 port: port,
                 online: false,
-                error: result
+                error: result,
             }
         }
 
